646c7d50216ae378b00f4ea1969f8d751dae3051
jawbreaker666 <andrasev96@web.de>
2022-01-10 10:45:46 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+//Angular eingebunden!
+import { Component } from '@angular/core';
+
+//HTML/SCSS/CSS eingebunden!
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+
+//Java Script
+export class AppComponent {
+  title = 'Angular_Projekt';
+
+
+
+
+}
