049dca4ffb0d5d1ef974fe007beb1aafe2bf4875
jawbreaker666 <andrasev96@web.de>
2022-01-11 09:53:40 +0100

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,33 +10,6 @@ import { Component, OnInit } from '@angular/core';
 
 
 //Java Script
-export class AppComponent implements OnInit{
-  images = ['laptop.jpg','codes.jpg','uni.jpg'];
-  headlines = ['Coding next Level!','Learning to read codes','Certificate teachers in the universitate'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit(){
-    this.updateImage();
-  }
-
-  updateImage() {
-    
-    setInterval(() => {
-
-    this.currentImage++;
-    this.currentImage = this.currentImage % this.images.length;
-    this.showImage = false;
-
-    setTimeout(()=>{
-      this.showImage = true;
-    }, 10);
-    }, 8000);
-  }
-  } 
-
-
-
-
+export class AppComponent{}
 
 
